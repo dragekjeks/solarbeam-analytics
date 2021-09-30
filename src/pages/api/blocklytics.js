f7ae5b451dd4988c70632e12c07f640d8923ceae
- <->
2021-09-29 21:25:06 -0300

    cors

--- src/pages/api/blocklytics.js
+++ src/pages/api/blocklytics.js
@@ -1,6 +1,26 @@
+import Cors from "cors";
 const { default: axios } = require("axios");
 
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["POST", "HEAD"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+
   const result = await axios.post(
     `http://subgraph.solarbeam.io/subgraphs/name/solarbeam/blocklytics`,
     req.body
