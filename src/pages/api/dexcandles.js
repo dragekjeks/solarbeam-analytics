076be126ba0633bf01b13e0970c5d263116b7c60
- <->
2021-09-29 23:42:51 -0300

    dexcandles

--- src/pages/api/dexcandles.js
+++ src/pages/api/dexcandles.js
@@ -1,6 +1,16 @@
+import NextCors from "nextjs-cors";
 const { default: axios } = require("axios");
 
 export default async function handler(req, res) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const result = await axios.post(
     `http://subgraph.solarbeam.io/subgraphs/name/solarbeam/dexcandles`,
     req.body
